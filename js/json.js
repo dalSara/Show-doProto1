8ba5e2414b46482c1f013cf4d0e7ad95132c6197
sillyTroll <saradalfalk@gmail.com>
2017-02-28 14:14:25 +0100

    jag å min json.. igen

--- js/json.js
+++ js/json.js
@@ -1,20 +1,57 @@
-function getJSONP(url, success) {
+$(function(){
 
-    var ud = '_' + +new Date,
-        script = document.createElement('script'),
-        head = document.getElementsByTagName('head')[0]
-               || document.documentElement;
+            //htmlObjekter
+            var $prevBtn;
+            var $thisWeekBtn;
+            var $nextBtn;
+            var $infoTest;
 
-    window[ud] = function(data) {
-        head.removeChild(script);
-        success && success(data);
-    };
 
-    script.src = url.replace('callback=?', 'callback=' + ud);
-    head.appendChild(script);
 
-}
 
-getJSONP('info.json', function(data){
-    console.log(data);
-});
+            var init = function(){
+
+                var setHTMLObjects = function(){
+                    $prevBtn = $("#prevBtn");
+                    $thisWeekBtn = $("#thisWeekBtn");
+                    $nextBtnBtn = $("#nextBtnBtn");
+                    $infoTest = $("#infoTest").get(0);
+
+        }();//end set html objects
+
+
+
+            }();//end init
+
+            var events = [];
+
+
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: 'info.json',
+                success: 1,
+                }).done(function ( json ) {
+                    events = json[0].events;
+
+                console.log(events);
+            });
+
+
+
+     /*       function getInfoJson(url){}
+
+
+
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: infoUrl ,
+            data: data,
+            success: success
+            });
+
+        */
+
+        }());
+        //delete
