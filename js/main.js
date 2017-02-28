665be019097db1edbd00d624220f58b95da0bedb
Natalie Uranes <uranes743@hotmail.com>
2017-02-28 13:05:27 +0100

    Fjerna Angular. Lagt til jQuery .loade for å hente inn 'components'. Og flytta jQuery fra index.html til main.js.

--- js/main.js
+++ js/main.js
@@ -3,23 +3,33 @@ $(document).ready(function() {
     //HTML objects
     var $scrollToTopBtn;
 
+    //Set HTML objects
     var setHTMLObjects = function(){
-        $scrollToTopBtn = $(".scrollToTopBtn");
-    };
+        $scrollToTopBtn = $("#scrollToTopBtn");
+    };//end Set HTML objects
 
+    //Set events
     var setEvents = function(){
         $scrollToTopBtn.on("click", function(){
             scrollUp();
         });
-    };
+    };//end Set events
 
     //init
     var init = function(){
+        //--------GET COMPONENTS
+        $("#header").load("components/header.html");
+        $("#navigation").load("components/navigation.html");
+        $("#calender").load("components/calender.html");
+        //$("#calenderFlexbox").load("components/calender-flexbox.html");
+        $("#list").load("components/list.html");
+        //--------end GET COMPONENTS
+
         setHTMLObjects();
         setEvents();
     }();//end init
 
-    //SCROLL TO TOP
+    //--------SCROLL TO TOP
     //Check to see if the window is top if not then display button
     $(window).scroll(function(){
         if ($(this).scrollTop() > 100) {
@@ -34,5 +44,5 @@ $(document).ready(function() {
         $("html, body").animate({
             scrollTop : 0},800);
         return false;
-    }//end SCROLL TO TOP
+    }//--------end SCROLL TO TOP
 });
