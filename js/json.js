342473c126e3a83ba8b6478e19ffba69f80db42a
Sigrun Gjengset <sigrungjengset@Sigruns-MacBook-Pro.local>
2017-03-02 16:25:32 +0100

    Påmeldingsknapp lagt til. Duration på mobil lagt til.

--- js/json.js
+++ js/json.js
@@ -58,21 +58,21 @@ $(function(){
 
     function showInfo(json){
         $(".JStrack1")
-            .append(events[0].title + ("<br>") + " Room: ", events[0].room);
+            .append(events[0].title + ("<br>") + ("<div class='rooms'>") + " Room: " + events[0].room + ("</div>") + ("<div class='duration'>") + "Duration: " + events[0].duration + ("</div>"));
         $(".JStrack2")
-            .append(events[1].title + ("<br>") + " Room: ", events[1].room);
+            .append(events[1].title + ("<br>") + ("<div class='rooms'>") + " Room: " + events[1].room + ("</div>") + ("<div class='duration'>") + "Duration: " + events[1].duration + ("</div>"));
         $(".JStrack3")
-            .append(events[2].title + ("<br>") + " Room: ", events[2].room);
+            .append(events[2].title + ("<br>") + ("<div class='rooms'>") + " Room: " + events[2].room + ("</div>") + ("<div class='duration'>") + "Duration: " + events[2].duration + ("</div>"));
         $(".JStrack4")
-            .append(events[3].title + ("<br>") + " Room: ", events[3].room);
+            .append(events[3].title + ("<br>") + ("<div class='rooms'>") + " Room: " + events[3].room + ("</div>") + ("<div class='duration'>") + "Duration: " + events[3].duration + ("</div>"));
         $(".JStrack5")
-            .append(events[4].title + ("<br>") + " Room: ", events[4].room);
+            .append(events[4].title + ("<br>") + ("<div class='rooms'>") + " Room: " + events[4].room + ("</div>") + ("<div class='duration'>") + "Duration: " + events[4].duration + ("</div>"));
         $(".JStrack6")
-            .append(events[5].title + ("<br>") + " Room: ", events[5].room);
+            .append(events[5].title + ("<br>") + ("<div class='rooms'>") + " Room: " + events[5].room + ("</div>") + ("<div class='duration'>") + "Duration: " + events[5].duration + ("</div>"));
         $(".JStrack7")
-            .append(events[6].title + ("<br>") + " Room: ", events[6].room);
-        $(".JStrack9")
-            .append(events[7].title + ("<br>") + " Room: ", events[7].room);
+            .append(events[6].title + ("<br>") + ("<div class='rooms'>") + " Room: " + events[6].room + ("</div>") + ("<div class='duration'>") + "Duration: " + events[6].duration + ("</div>"));
+        $(".JStrack8")
+            .append(events[7].title + ("<br>") + ("<div class='rooms'>") + " Room: " + events[7].room + ("</div>") + ("<div class='duration'>") + "Duration: " + events[7].duration + ("</div>"));
     };
 
     function showThisWeek(json){
