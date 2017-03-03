b0176402c27daa047f8bd595014393c5facd5a50
Natalie Uranes <uranes743@hotmail.com>
2017-03-03 09:55:56 +0100

    showEventList();

--- js/json.js
+++ js/json.js
@@ -5,6 +5,7 @@ $(function(){
     var $thisWeekBtn;
     var $nextWeekBtn;
     var $infoTest;
+    var $listContent;
 
 
     var init = function(){
@@ -14,8 +15,8 @@ $(function(){
             $thisWeekBtn = $("#thisWeekBtn");
             $nextWeekBtn = $("#nextWeekBtn");
             $infoTest = $("#infoTest").get(0);
+            $listContent = $("#listContent");
         }();//end set html objects
-    }();//end init
 
         var setEvents = function(){
             $thisWeekBtn.on("click", function(){
@@ -27,20 +28,18 @@ $(function(){
             $prevWeekBtn.on("click", function(){
                 showPrevWeek();
             });
-
-
-
-
+            /*$moreInforLink.on("click", function(){
+                $moreInfo.toggle();
+            });*/
         };//end setEvents
-
-
+    }();//end init
 
     var events = [];
     var index = [1]
 
-
     $.ajax({
         type: "GET",
+        data: {hendelser: JSON.stringify(events) },
         dataType: "json",
         url: 'info.json',
         success: 1
@@ -50,6 +49,8 @@ $(function(){
         showThisWeek(json);
         showNextWeek(json);
         showPrevWeek(json);
+
+        showEventList(json);
         // console.log(events);
     })
         .fail(function (XMLHttpRequest, textStatus, errorThrown){
@@ -74,19 +75,69 @@ $(function(){
             .append(events[6].title + ("<br>") + ("<div class='rooms'>") + " Room: " + events[6].room + ("</div>") + ("<div class='duration'>") + "Duration: " + events[6].duration + ("</div>"));
         $(".JStrack8")
             .append(events[7].title + ("<br>") + ("<div class='rooms'>") + " Room: " + events[7].room + ("</div>") + ("<div class='duration'>") + "Duration: " + events[7].duration + ("</div>"));
-    };
+    }
 
     function showThisWeek(json){
         json[1];
-    };//end thisPrevWeek
+    }//end thisPrevWeek
 
 
     function showPrevWeek(json){
         json[0];
-    };//end ShowPrevWeek
+    }//end ShowPrevWeek
 
     function showNextWeek(json){
 
-    };//end ShowNextWeek
+    }//end ShowNextWeek
 
-}());
+    function showEventList(json){
+
+        console.log(json[index]);
+
+        $(json[index].events).each(function(){
+
+            //--------EVENT
+            var $title = $("<h4>").html(this.title);
+            var $room = $("<p>").html("Room: " + this.room);
+            var $numberOfPartic = $("<p>").html("Number of participants: " + this.numberOfPartic);
+            var $prereq = $("<p>").html("Prerequisites: " + this.prereq);
+            var $expect = $("<p>").html("What to expect: " + this.expect);
+            var $host = $("<p>").html("Host: " + this.host);
+            var $whoJoin = $("<p>").html("Who should join? " + this.whoJoin);
+            var $image = $("<img>").html(this.image)
+            .addClass("image-to-cut");
+            var $elseText = $("<p>").html("Other information: " + this.elseText);
+            var $type = $("<p>").html("Size: " + this.type);
+            var $startTime = $("<p>").html("Starts: " + this.startTime);
+            //--------end EVENT
+
+            //--------HTML elements
+            var $event = $("<li>")
+                .css("background-color", "lightgray"); //Test color
+
+            var $figure = $("<figure>")
+                .addClass("cut-image"); //For image
+
+            var $moreInforLink = $("<a href=''>"); //Link for more info
+            var $moreInfo = $("<div>"); //More info
+            //--------end HTML elements
+
+            $event.append($title/*
+                $figure.append($image),
+                $moreInforLink.append($title),
+                $moreInfo.append(
+                    $room,
+                    $numberOfPartic,
+                    $prereq,
+                    $expect,
+                    $host,
+                    $whoJoin,
+                    $elseText,
+                    $type,
+                    $startTime)*/
+                         );
+
+            $listContent.append($event);
+        }); //end each loop
+    }
+});
