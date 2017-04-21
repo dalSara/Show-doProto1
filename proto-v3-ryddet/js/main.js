49e5e8bfdc5cbc53eaef60e186af0c2426744277
Sigrun Gjengset <sigrungjengset@Sigruns-MacBook-Pro.local>
2017-04-21 11:52:14 +0200

    Added svg-icons

--- proto-v3-ryddet/js/main.js
+++ proto-v3-ryddet/js/main.js
@@ -474,7 +474,7 @@ $(function() {
 
     function showInfo(json){
         $(".JStrack1")
-            .append(("<a href='#JsScroll1' class='scrollToStyle'>") + "<h4>" + events[0].title + "</h4>" + ("<div class='rooms '><i class='fa fa-map-marker' aria-hidden='true'></i> ") + events[0].room + ("</div>") + ("<div class='duration'>") + "Duration: " + events[0].duration + ("</div>") + ("</a>"));
+            .append("<h4>" + events[0].title + "</h4>" + ("<div class='rooms '><i class='fa fa-map-marker' aria-hidden='true'></i> ") + events[0].room + ("</div>") + ("<div class='duration'>") + "Duration: " + events[0].duration + ("</div>"));
         $(".JStrack2")
             .append(("<a href='#JsScroll2' class='scrollToStyle'>") + "<h4>" + events[1].title + "</h4>" + ("<div class='rooms '><i class='fa fa-map-marker' aria-hidden='true'></i> ") + events[1].room + ("</div>") + ("<div class='duration'>") + "Duration: " + events[1].duration + ("</div>") + ("</a>"));
         $(".JStrack3")
@@ -656,4 +656,65 @@ $(function() {
             scrollTop : 0},800);
         return false;
     };//--------end SCROLL TO TOP
+
+
+    var increment2 = 10;
+        $(document).on("change", ".checkbox", function() {
+            $('.number').html('' + (increment2 += this.checked ? 1 : -1) + '');
+        })
+
+        // Hide Header on on scroll down
+        var didScroll;
+        var lastScrollTop = 0;
+        var delta = 5;
+        var navbarHeight = $('header').outerHeight();
+
+        $(window).scroll(function(event) {
+            didScroll = true;
+        });
+
+        setInterval(function() {
+            if (didScroll) {
+                hasScrolled();
+                didScroll = false;
+            }
+        }, 250);
+
+        function hasScrolled() {
+            var st = $(this).scrollTop();
+
+            // Make sure they scroll more than delta
+            if (Math.abs(lastScrollTop - st) <= delta)
+                return;
+
+            // If they scrolled down and are past the navbar, add class .nav-up.
+            // This is necessary so you never see what is "behind" the navbar.
+            if (st > lastScrollTop && st > navbarHeight) {
+                // Scroll Down
+                $('header').removeClass('nav-down').addClass('nav-up');
+            } else {
+                // Scroll Up
+                if (st + $(window).height() < $(document).height()) {
+                    $('header').removeClass('nav-up').addClass('nav-down');
+                }
+            }
+
+            lastScrollTop = st;
+        }
+
+        $(".scroll").click(function(event) {
+            event.preventDefault();
+            //calculate destination place
+            var dest = 0;
+            if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+                dest = $(document).height() - $(window).height();
+            } else {
+                dest = $(this.hash).offset().top;
+            }
+            //go to destination
+            $('html,body').animate({
+                scrollTop: dest
+            }, 800, 'swing');
+        });
+
 });
